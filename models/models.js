c2c449e8fd9d2ec08e10ef43d673d53e07689c70
César Daniel Meneses Guevara <cdanielmg200@gmail.com>
2015-06-06 19:16:41 -0500

    Soporte para una lista de pregunta
    Soporte para una lista de pregunta

--- models/models.js
+++ models/models.js
@@ -32,14 +32,17 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; // exportar definicio de la tabla quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en BBDD
-sequelize.sync().success(function(){
+sequelize.sync().then(function(){
   //success(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().success(function(count){
     if(count===0){// la tabla se inicializa solo si esta vacia
       Quiz.create({ pregunta:'Capital de Italia',
                     respuesta:'Roma'
+                  });
+      Quiz.create({ pregunta:'Capital de Portugal',
+                    respuesta:'Lisboa'
                   })
-                  .success(function(){console.log('Base de datos inicializada');});
+      .then(function(){console.log('Base de datos inicializada');});
     };
   });
 });
--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,8 @@ router.get('/author', function(req, res) {
   res.render('author', { author: 'César Daniel Meneses Guevara' });
 });
 
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
+router.get('/quizes',quizController.index);
+router.get('/quizes/:quizId(\\d+)',quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
 
 module.exports = router;
