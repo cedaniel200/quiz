91c51ead3459989e592a0ea15ffe8eb0d7ce9916
César Daniel Meneses Guevara <cdanielmg200@gmail.com>
2015-06-08 17:26:16 -0500

    Se agrega el tema al cual pertenece la pregunta
    Se agrega el tema al cual pertenece la pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -48,7 +48,7 @@ exports.answer = function(req,res){
 exports.new = function(req, res){
   var quiz = models.Quiz.build(
     // Crea Objeto quiz
-    {pregunta:"Pregunta", respuesta: "Respuesta"}
+    {pregunta:"Pregunta", respuesta: "Respuesta", tema:""}
   );
   res.render('quizes/new', {quiz:quiz, errors:[]});
 };
@@ -65,7 +65,7 @@ exports.create = function(req, res){
         res.render('quizes/new', {quiz:quiz,errors:err.errors});
       }else{
         // guardar en la DB los campos pregunta y respuesta de quiz
-        quiz.save({feilds:["pregunta","respuesta"]}).then(function(){
+        quiz.save({feilds:["pregunta","respuesta","tema"]}).then(function(){
           res.redirect('/quizes'); // Redireccion HTTP (URL relativo) lista de preguntas
         });
       }
@@ -80,6 +80,7 @@ exports.edit = function(req,res){
 exports.update = function(req, res){
   req.quiz.pregunta = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
 
   req.quiz
   .validate()
@@ -88,7 +89,7 @@ exports.update = function(req, res){
         if(err){
           res.render('quizes/edit',{quiz: req.quiz, errors:err.errors});
         }else{
-          req.quiz.save({feilds:["pregunta","respuesta"]})
+          req.quiz.save({feilds:["pregunta","respuesta","tema"]})
           .then(function(){
             res.redirect('/quizes'); // Redireccion HTTP (URL relativo) lista de preguntas
           });
