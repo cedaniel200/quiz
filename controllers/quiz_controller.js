fc07b70bbbf9fe7ece8f81cdad96d70ffe063ed6
César Daniel Meneses Guevara <cdanielmg200@gmail.com>
2015-06-08 13:55:08 -0500

    Crear preguntas
    Permite crear nuevas preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -23,13 +23,13 @@ exports.index = function(req, res){
   search = '%'+search+'%';
 
   models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes){
-    res.render('quizes/index.ejs',{quizes:quizes});
+    res.render('quizes/index.ejs',{quizes:quizes, errors:[]});
   }).catch(function(error){next(error);});
 };
 
 // GET /quizes/question
 exports.show = function(req,res){
-  res.render('quizes/show',{quiz:req.quiz});
+  res.render('quizes/show',{quiz:req.quiz, errors:[]});
 };
 
 // GET /quizes/answer
@@ -38,5 +38,37 @@ exports.answer = function(req,res){
   if(req.query.respuesta === req.quiz.respuesta){
     resultado = 'Correcto';
   }
-  res.render('quizes/answer',{quiz:req.quiz,respuesta:resultado});
+  res.render('quizes/answer',{
+      quiz:req.quiz,
+      respuesta:resultado,
+      errors:[]
+    });
+};
+
+exports.new = function(req, res){
+  var quiz = models.Quiz.build(
+    // Crea Objeto quiz
+    {pregunta:"Pregunta", respuesta: "Respuesta"}
+  );
+  res.render('quizes/new', {quiz:quiz, errors:[]});
+};
+
+// POST /quizes/create
+exports.create = function(req, res){
+  var quiz = models.Quiz.build(req.body.quiz);
+
+  quiz
+  .validate()
+  .then(
+    function(err){
+      if(err){
+        res.render('quizes/new', {quiz:quiz,errors:err.errors});
+      }else{
+        // guardar en la DB los campos pregunta y respuesta de quiz
+        quiz.save({feilds:["pregunta","respuesta"]}).then(function(){
+          res.redirect('/quizes'); // Redireccion HTTP (URL relativo) lista de preguntas
+        });
+      }
+    }
+  );
 };
--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,22 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors:[] });
 });
 
 /* GET author page. */
 router.get('/author', function(req, res) {
-  res.render('author', { author: 'César Daniel Meneses Guevara' });
+  res.render('author', { author: 'César Daniel Meneses Guevara', errors:[] });
 });
 
 // AUTOLOAD de comandos con :quizId
 router.param('quizId', quizController.load); // AUTOLOAD :quizId
 
 // Definición de rutas del /quizes
-router.get('/quizes',quizController.index);
-router.get('/quizes/:quizId(\\d+)',quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
+router.get('/quizes',                         quizController.index);
+router.get('/quizes/:quizId(\\d+)',           quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',    quizController.answer);
+router.get('/quizes/new',                     quizController.new);
+router.post('/quizes/create',                  quizController.create);
 
 module.exports = router;
